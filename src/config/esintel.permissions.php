cc533edeeedfdf910ed2f6d2030cfcaf7fc5ff8f
Felix Dietzsch <felix.dietzsch@mykolab.ch>
2019-12-29 21:15:02 +0100

    add permission roles

--- src/config/esintel.permissions.php
+++ src/config/esintel.permissions.php
@@ -2,6 +2,12 @@
 
 return [
     "esintel" => [
-
+        "view_char_es",
+        "view_char_intel_category",
+        "view_char_intel_description",
+        "add_char",
+        "edit_char",
+        "delete_char",
+        "configure"
     ],
 ];
